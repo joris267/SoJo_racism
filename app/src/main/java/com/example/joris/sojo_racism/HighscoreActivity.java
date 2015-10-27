2f1ce2ca6527e4d9182e90b8915e6e2a788cf498
joris <jorisschefold@gmail.com>
2015-10-27 19:33:04 +1100

    Added activities and started on game screen

---
+++ app/src/main/java/com/example/joris/sojo_racism/HighscoreActivity.java
@@ -0,0 +1,13 @@
+package com.example.joris.sojo_racism;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
